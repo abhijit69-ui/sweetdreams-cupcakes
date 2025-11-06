8a09fae3fd925ce09918e7250cd5bffd29a43167
Abhijit Nath <abhijitnath1a1@gmail.com>
2025-11-06 13:13:07 +0530

    shared navItems

--- src/components/elements/Navbar.tsx
+++ src/components/elements/Navbar.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
-
+import Container from '../../utils/Container';
+import { Link } from 'react-scroll';
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className=''>
+      <Container>
+        <nav className='flex items-center justify-between'>
+          {/* Logo */}
+          <Link
+            to='hero'
+            smooth={true}
+            duration={1000}
+            offset={-80}
+            className='flex items-center gap-2 cursor-pointer'
+          >
+            <span className='text-xl font-primary font-bold'>Sweet Dreams</span>
+          </Link>
+        </nav>
+      </Container>
+    </header>
+  );
 };
 
 export default Navbar;
---
+++ src/components/shared/navItems.ts
@@ -0,0 +1,6 @@
+export const navItems = [
+  { href: '#treats', label: 'Our Treats' },
+  { href: '#spotlight', label: 'Spotlight' },
+  { href: '#collection', label: 'Collection' },
+  { href: '#bakersnote', label: 'Bakers Note' },
+];
