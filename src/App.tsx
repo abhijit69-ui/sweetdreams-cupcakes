05b6cef66d46323ca19baf0093780737d580bb20
Abhijit Nath <abhijitnath1a1@gmail.com>
2025-11-06 11:48:14 +0530

    Layout and Folder Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Layout from './components/Layout';
+import Hero from './components/sections/Hero';
+
 const App = () => {
-  return <div className='text-7xl font-primary font-semibold'>App</div>;
+  return (
+    <Layout title='SweetDreams Cafe'>
+      <Hero />
+    </Layout>
+  );
 };
 
 export default App;
---
+++ src/components/sections/Hero.tsx
@@ -0,0 +1,5 @@
+const Hero = () => {
+  return <div className='text-6xl font-primary font-semibold'>Hero</div>;
+};
+
+export default Hero;
