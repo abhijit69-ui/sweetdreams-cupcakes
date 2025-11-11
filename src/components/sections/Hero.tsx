ecd704b02d7ee69e96ad1463d205074b692b438b
natsu0001 <talukdarabhishek687@gmail.com>
2025-11-11 14:07:58 +0530

    bg

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -9,22 +9,29 @@ const Hero: React.FC = () => {
   ];
 
   return (
-          <section className="relative w-full h-[calc(100vh-4rem)] overflow-hidden pt-16 ">
-         
-          <div className="absolute inset-0 bg-[url('/assets/bg_cloud.jpg')] bg-cover bg-center opacity-50 -z-10" />
+        <section className="relative w-full h-[calc(100vh-4rem)] overflow-hidden pt-16">
+  
+  <div className="absolute inset-0 bg-[url('/assets/bg_cloud.jpg')] bg-cover bg-center opacity-50 -z-20" />
 
-           <div className="relative z-10 flex flex-col items-center justify-center text-center h-full px-4">
-           
-             <h2 className="text-4xl sm:text-5xl md:text-6xl font-primary font-extrabold text-lavender leading-tight drop-shadow-[0_4px_10px_rgba(255,192,203,0.3)]">
-              Where Sweet Dreams <br />
-              <span className="text-pink-500">Become Delicious Reality</span>
-             </h2>
+ 
+  <div className="absolute inset-0 flex justify-center items-center -z-10 pointer-events-none">
+    <img
+      src="/assets/bgcake.png"   
+      alt="Cupcake background"
+      className="w-[300px] sm:w-[400px] md:w-[500px] lg:w-[600px] opacity-70 object-contain mt-17"
+    />
+  </div>
+
+  
+  <div className="relative z-10 flex flex-col items-center justify-center text-center h-full px-4">
+    <h2 className="text-4xl sm:text-5xl md:text-6xl font-primary font-extrabold text-lavender leading-tight drop-shadow-[0_4px_10px_rgba(255,192,203,0.3)]">
+      Where Sweet Dreams <br />
+      <span className="text-pink-500">Become Delicious Reality</span>
+    </h2>
+  </div>
+</section>
 
 
-             {/* Muffins layout */}
-             
-         </div>
-      </section>
    );
 };
 
